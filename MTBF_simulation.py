c5603438b663bdd110fac9ef37cad5753fe5d488
Li-Syuan <45783364+Li-Syuan@users.noreply.github.com>
2023-03-25 14:23:35 +0800

    add pd.dataframe

--- MTBF_simulation.py
+++ MTBF_simulation.py
@@ -1,4 +1,7 @@
 import random
+import numpy as np
+import pandas as pd
+
 random.seed(123)
 # Define the number of machines to simulate
 num_machines = 10
@@ -26,9 +29,12 @@ mean_mtbf = [sum(data)/len(data) for data in machine_data]
 # Determine the index of the machine with the lowest mean MTBF
 worst_machine_index = mean_mtbf.index(min(mean_mtbf))
 
+machine_data = pd.DataFrame(machine_data).round()
+
+machine_data.to_csv('data.csv')
 # Print the results
 print("Machine performance data:")
 for i in range(num_machines):
-    _ = f'Machine {i+1}, Random MTBF: {machine_mtbf[i]}, Real MTBF: {round(mean_mtbf[i], 2)} hours'
-    print(_)
+    print(f'Machine {i+1}, MTBF: {round(mean_mtbf[i], 2)} hours')
+    
 print("Worst performing machine is machine", worst_machine_index+1)
